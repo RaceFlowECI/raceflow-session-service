872bba0ec40a5784e19b9b4b389a9b618f2b3ead
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2026-07-03 20:00:00 -0500

    feat: initialize Spring Boot 3.2 project structure

---
+++ src/test/java/edu/eci/arsw/raceflow/session/SessionApplicationTests.java
@@ -0,0 +1,10 @@
+package edu.eci.arsw.raceflow.session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionApplicationTests {
+    @Test
+    void contextLoads() {}
+}
